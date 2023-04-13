28c2fc3b685794f18764c1d0c7e77bc8dfb41a05
Tazmaniego <DiegoAbrahamMontiel@gmail.com>
2023-04-12 19:08:58 -0700

    ;

--- test/specs/KeyPresses.spec.js
+++ test/specs/KeyPresses.spec.js
@@ -3,9 +3,9 @@ import KeyPressesPage from "../pageobjects/KeyPresses.page.js";
 describe ('My KeyPresses application', () => {
     it('should display what is entered into the input', async () => {
         await KeyPressesPage.open();
-        await browser.pause(3000)
+        await browser.pause(3000);
         await KeyPressesPage.pressAkey('C');
         await expect(KeyPressesPage.keyPressResult).toHaveTextContaining('You entered: C');
-        await browser.pause(3000)
+        await browser.pause(3000);
     });
 });
